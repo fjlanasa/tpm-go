677d2f62dec60d0a9170a2d15c0b052f52573853
Frank LaNasa <fjlanasa@gmail.com>
2025-02-18 07:25:19 -0500

    update

---
+++ api/v1/events/events.go
@@ -0,0 +1,77 @@
+package events
+
+// Base interface for all events
+type Event interface {
+	GetAgencyId() string
+	GetAttributes() map[string]any
+}
+
+func (e *FeedMessageEvent) GetAttributes() map[string]any {
+	return map[string]any{
+		"agency_id": e.AgencyId,
+		"timestamp": e.FeedMessage.Header.GetTimestamp(),
+	}
+}
+
+func (e *VehiclePositionEvent) GetAttributes() map[string]any {
+	return map[string]any{
+		"agency_id":     e.AgencyId,
+		"route_id":      e.RouteId,
+		"trip_id":       e.TripId,
+		"direction_id":  e.DirectionId,
+		"stop_id":       e.StopId,
+		"stop_sequence": e.StopSequence,
+		"latitude":      e.Latitude,
+		"longitude":     e.Longitude,
+	}
+}
+
+func (e *StopEvent) GetAttributes() map[string]any {
+	return map[string]any{
+		"agency_id":     e.AgencyId,
+		"route_id":      e.RouteId,
+		"trip_id":       e.TripId,
+		"direction_id":  e.DirectionId,
+		"stop_id":       e.StopId,
+		"stop_sequence": e.StopSequence,
+		"type":          e.EventType,
+		"timestamp":     e.Timestamp,
+	}
+}
+
+func (e *DwellTimeEvent) GetAttributes() map[string]any {
+	return map[string]any{
+		"agency_id":          e.AgencyId,
+		"route_id":           e.RouteId,
+		"trip_id":            e.TripId,
+		"direction_id":       e.DirectionId,
+		"stop_id":            e.StopId,
+		"stop_sequence":      e.StopSequence,
+		"dwell_time_seconds": e.DwellTimeSeconds,
+		"timestamp":          e.Timestamp,
+	}
+}
+
+func (e *HeadwayTimeEvent) GetAttributes() map[string]any {
+	return map[string]any{
+		"agency_id":            e.AgencyId,
+		"route_id":             e.RouteId,
+		"direction_id":         e.DirectionId,
+		"stop_id":              e.StopId,
+		"headway_time_seconds": e.HeadwayBranchSeconds,
+		"timestamp":            e.Timestamp,
+	}
+}
+
+func (e *TravelTimeEvent) GetAttributes() map[string]any {
+	return map[string]any{
+		"agency_id":           e.AgencyId,
+		"route_id":            e.RouteId,
+		"trip_id":             e.TripId,
+		"direction_id":        e.DirectionId,
+		"origin_stop_id":      e.FromStopId,
+		"destination_stop_id": e.ToStopId,
+		"travel_time_seconds": e.TravelTimeSeconds,
+		"timestamp":           e.Timestamp,
+	}
+}
--- internal/sinks/log_sink.go
+++ internal/sinks/log_sink.go
@@ -44,62 +44,22 @@ func (s *LogSink) doSink(ctx context.Context) {
 		select {
 		case <-ctx.Done():
 			return
-		case event := <-s.in:
-			switch event := event.(type) {
-			case *events.VehiclePositionEvent:
-				s.logger.Log(ctx, s.level, "vehicle-position",
-					"agency_id", event.GetAgencyId(),
-					"route_id", event.GetTripId(),
-					"trip_id", event.GetTripId(),
-					"direction_id", event.GetDirectionId(),
-					"stop_id", event.GetStopId(),
-					"stop_sequence", event.GetStopSequence(),
-					"latitude", event.GetLatitude(),
-					"longitude", event.GetLongitude(),
-				)
-			case *events.StopEvent:
-				s.logger.Log(ctx, s.level, "stop",
-					"agency_id", event.GetAgencyId(),
-					"route_id", event.GetRouteId(),
-					"trip_id", event.GetTripId(),
-					"direction_id", event.GetDirectionId(),
-					"stop_id", event.GetStopId(),
-					"stop_sequence", event.GetStopSequence(),
-					"type", event.GetEventType(),
-					"timestamp", event.GetTimestamp(),
-				)
-			case *events.DwellTimeEvent:
-				s.logger.Log(ctx, s.level, "dwell",
-					"agency_id", event.GetAgencyId(),
-					"route_id", event.GetRouteId(),
-					"trip_id", event.GetTripId(),
-					"direction_id", event.GetDirectionId(),
-					"stop_id", event.GetStopId(),
-					"stop_sequence", event.GetStopSequence(),
-					"dwell_time_seconds", event.GetDwellTimeSeconds(),
-					"timestamp", event.GetTimestamp(),
-				)
-			case *events.HeadwayTimeEvent:
-				s.logger.Log(ctx, s.level, "headway",
-					"agency_id", event.GetAgencyId(),
-					"route_id", event.GetRouteId(),
-					"direction_id", event.GetDirectionId(),
-					"stop_id", event.GetStopId(),
-					"headway_time_seconds", event.GetHeadwayBranchSeconds(),
-					"timestamp", event.GetTimestamp(),
-				)
-			case *events.TravelTimeEvent:
-				s.logger.Log(ctx, s.level, "travel-time",
-					"agency_id", event.GetAgencyId(),
-					"route_id", event.GetRouteId(),
-					"trip_id", event.GetTripId(),
-					"direction_id", event.GetDirectionId(),
-					"origin_stop_id", event.GetFromStopId(),
-					"destination_stop_id", event.GetToStopId(),
-					"travel_time_seconds", event.GetTravelTimeSeconds(),
-					"timestamp", event.GetTimestamp(),
-				)
+		case msg, ok := <-s.in:
+			if !ok {
+				return
 			}
+			event, ok := msg.(events.Event)
+			if !ok {
+				s.logger.Log(ctx, slog.LevelWarn, "invalid event", "event", msg)
+				continue
+			}
+			var args []any
+			if attrs := event.GetAttributes(); len(attrs) > 0 {
+				for k, v := range attrs {
+					args = append(args, k, v)
+				}
+			}
+			s.logger.Log(ctx, s.level, "event", args...)
 		}
 	}
 }
